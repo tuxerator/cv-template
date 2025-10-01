1c2a755c122947b3274ad1ea04893d7d6311fbea
Jakob Sanowski <jakob.sanowski@sanbach.de>
2025-10-01 14:40:37 +0200

    nix init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            typst
+            tinymist
+          ];
+        };
+      }
+    );
+}
